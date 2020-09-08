bb706caca0070536d6205b2e2ccec8918376d507
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-09-08 18:01:23 +0100

    construct file inspector class

--- lib/my_inspect.rb
+++ lib/my_inspect.rb
@@ -1,5 +1,21 @@
 class Inspector
   def initialize(inputfile)
     @inputfile = inputfile
+    @flaw_point = []
+  end
+  
+  def inspect_file(first, last)
+    @flaw_point = []
+    
+    File.open(@inputfile).readlines.each_with_index do |line, idx|
+      if line.rindex(first).nil? && !line.rindex(last).nil? || 
+        !line.rindex(first).nil? && line.rindex(last).nil?
+        @flaw_point.push(idx)
+      end
+      @flaw_point.push(idx) if line.rindex(first) > line.rindex(last)
+      @flaw_point.push(idx) if line.scan(first).count != line.scan(last).count
+    end
+    File.open(@inputfile).close
+    @flaw_point.uniq
   end
 end
