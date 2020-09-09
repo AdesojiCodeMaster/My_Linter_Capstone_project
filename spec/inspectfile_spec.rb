ca9bf7054d386dbc35904772f32ee0021abf0690
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-09-09 01:30:32 +0100

    Add test cases to test inspect_file method

---
+++ spec/inspectfile_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/my_inspect.rb'
+describe Inspector do
+  context '#inspect_file' do
+  let(:test_file){Inspector.new('test/test.rb')}
+    it 'returns an array with point where flaw occurred' do
+      expect(test_file.inspect_file(/\{/, /\}/).class).to eq(Array)
+    end
+    it 'returns an array with point where flaw occurred' do
+      expect(test_file.inspect_file(/\(/, /\)/).class).to eq(Array)
+    end
+    it 'returns an array with point where flaw occurred' do
+      expect(test_file.inspect_file(/\[/, /\]/).class).to eql(Array)
+    end
+    it 'returns error when no argument is given' do
+        expect{test_file.inspect_file}.to raise_error(ArgumentError)
+      end
+  end
+end
