6cd61af2db843139fdfbf389bd79eaf9c3d6b9b5
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-09-09 16:01:54 +0100

    Adjust initialize method

--- lib/my_inspect.rb
+++ lib/my_inspect.rb
@@ -1,21 +1,20 @@
 class Inspector
   def initialize(inputfile)
     @inputfile = inputfile
-    @flaw_point = []
   end
 
   def inspect_file(first, last) # rubocop:disable Metrics/CyclomaticComplexity
-    @flaw_point = []
+    flaw_point = []
 
     File.open(@inputfile).readlines.each_with_index do |line, idx|
       if line.rindex(first).nil? && !line.rindex(last).nil? ||
          !line.rindex(first).nil? && line.rindex(last).nil?
-        @flaw_point.push(idx)
+        flaw_point.push(idx)
       end
-      @flaw_point.push(idx) if line.rindex(first).to_i > line.rindex(last).to_i
-      @flaw_point.push(idx) unless line.scan(first).count == line.scan(last).count
+      flaw_point.push(idx) if line.rindex(first).to_i > line.rindex(last).to_i
+      flaw_point.push(idx) unless line.scan(first).count == line.scan(last).count
     end
     File.open(@inputfile).close
-    @flaw_point.uniq
+    flaw_point.uniq
   end
 end
